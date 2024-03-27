1dad6126aa6ceebab8f3ec05a3529025edf3918e
betulioo <luis.nunez@bootcamp-upgrade.com>
2024-03-27 10:57:57 +0100

    second

--- 4.map/exercise-3.js
+++ 4.map/exercise-3.js
@@ -1,17 +1,42 @@
 // Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y añade al valor de .name el string ' (Visitado)' cuando el valor de la propiedad isVisited = true.
 
 // ```js
-const cities = [{isVisited:true, name: 'Tokyo'}, {isVisited:false, name: 'Madagascar'},{isVisited:true, name: 'Amsterdam'}, {isVisited:false, name: 'Seul'}];
+const ciudades = [
+  { isVisited: true, name: "Tokyo" },
+  { isVisited: true, name: "Madagascar" },
+  { isVisited: true, name: "Amsterdam" },
+  { isVisited: false, name: "Seul" },
+];
 // ```
 
-const newArr = cities.map(citie => {
-    
-    if (citie.isVisited === true) {
-        const visitedCitie = `${citie.name} ("visitado")`;
-        return visitedCitie;
-    } else {
-        return citie.name
-    }
-})
+//confuncion normal
+const nuevaLista = ciudades.map(function (ciudad) {
+  if (ciudad.isVisited === true) {
+    ciudad.name = ciudad.name + "(Visitado)";
+  }
 
-console.log(newArr);
+  return ciudad;
+});
+
+console.log(nuevaLista);
+
+//con funcionArrow
+// const nuevaLista2 = ciudades.map((ciudad) => {
+//   if (ciudad.isVisited === true) {
+//     ciudad.name = ciudad.name + "(Visitado)";
+//   }
+
+//   return ciudad;
+// });
+// console.log(nuevaLista2);
+// const newArr = cities.map(citie => {
+
+//     if (citie.isVisited === true) {
+//         const visitedCitie = `${citie.name} ("visitado")`;
+//         return visitedCitie;
+//     } else {
+//         return citie.name
+//     }
+// })
+
+// console.log(newArr);
